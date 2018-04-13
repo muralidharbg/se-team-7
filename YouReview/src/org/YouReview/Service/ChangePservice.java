753c945e12a4ea80f30cbe1a090fe620842f57fb
drcod3 <33334625+drcod3@users.noreply.github.com>
2018-04-12 22:36:13 -0400

    Add files via upload
    Change Password functionality has been implemented

---
+++ YouReview/src/org/YouReview/Service/ChangePservice.java
@@ -0,0 +1,92 @@
+package org.YouReview.Service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ChangePservice {
+
+	
+	public Connection getConnection() throws Exception{
+		Connection conn = null;
+		String url = "jdbc:mysql://localhost:3306/YouReview_DB?useSSL=false";
+		String driver = "com.mysql.jdbc.Driver";
+		try {
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url,"root","mysqldb123");
+		}
+		catch(SQLException sqle) {
+			System.out.println("SQLException: Unable to open connection to db: "+sqle.getMessage());
+		
+			throw sqle;
+		}
+		catch(Exception e) {
+		      System.out.println("Exception: Unable to open connection to db: "+e.getMessage());
+		      throw e;
+		   }
+		return conn;
+	}
+	
+	public boolean changePassword(String newpassWord, String userName) throws Exception{
+		
+		Connection conn = null;
+		boolean status = false;
+		try
+		{
+			conn = getConnection();
+			PreparedStatement pst = null;
+			Statement st = conn.createStatement();
+//			String sql_statement ="insert into Users (User_PassWord) values (\'"+newpassWord+"\')";
+			String sql_statement  ="update Users set User_PassWord = '"+newpassWord+"' where User_UserName = '"+userName+"'";
+			st.executeUpdate(sql_statement);
+			status = true;
+		}
+		
+		catch(Exception e)
+		{
+			 System.out.println("validateLogon: Error while changing password: "+e.getMessage());
+				throw e;
+		}
+		
+		finally {
+		   	conn.close();
+		  }
+
+		return status;
+	}
+	
+	
+	public boolean authenticate(String userName,String oldpassWord) throws Exception
+	{
+		Connection conn = null;
+		boolean status = false;
+		
+		try {
+			conn = getConnection();
+			PreparedStatement pst = null;
+			pst = conn.prepareStatement("select * from Users where User_UserName=? and User_PassWord=?");
+			pst.setString(1, userName);
+			pst.setString(2, oldpassWord);
+			ResultSet rs = pst.executeQuery();
+			if(rs.next())
+			{
+				status = true;
+			}
+		}
+		catch(Exception e) {
+		    System.out.println("validateLogon: Error while validating authentication for chaning password: "+e.getMessage());
+			throw e;
+		 
+		  } 
+		finally {
+		   	conn.close();
+		  }
+		
+		return status;
+		
+	}
+
+}
