df736a5b1cc9af8c81274e4f817a2efa2c214c15
drcod3 <33334625+drcod3@users.noreply.github.com>
2018-04-12 22:34:52 -0400

    Add files via upload

---
+++ YouReview/src/org/YouReview/Servlets/ChangePassword.java
@@ -0,0 +1,93 @@
+package org.YouReview.Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.YouReview.Service.*;
+
+/**
+ * Servlet implementation class ChangePassword
+ */
+@WebServlet("/ChangePassword")
+public class ChangePassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+//    public ChangePassword() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String oldpassWord, newpassWord, conpassWord, userName;
+		boolean result = false;
+		
+		response.setContentType("text/html");
+		PrintWriter printer = response.getWriter();
+		
+		oldpassWord = request.getParameter("oldpassWord");
+		newpassWord = request.getParameter("newpassWord");
+	    conpassWord = request.getParameter("conpassWord");
+		userName = request.getParameter("userName");
+	   
+	    
+	    //LoginService ls = new LoginService();
+	    ChangePservice cp = new ChangePservice();
+	    try
+		{
+			if((!oldpassWord.isEmpty() || !newpassWord.isEmpty() || !conpassWord.isEmpty()) && (newpassWord.equals(conpassWord)) && !(oldpassWord.equals(newpassWord)))
+			{	
+				if(cp.authenticate(userName, oldpassWord))
+				{
+				result = cp.changePassword(newpassWord, userName);
+				}
+			}
+			else
+			{
+				RequestDispatcher rd = request.getRequestDispatcher("ChangePassword.jsp");
+				   printer.println("<font color=red>Please fill all the fields or make sure the credentials match</font>");
+				   rd.include(request, response);
+				   
+			}
+			
+		}
+		catch(Exception e)
+		{	
+			System.out.println("Error");;
+		}
+	    
+	    
+	    
+	    if(result)
+		{
+	    	HttpSession session = request.getSession();
+	        session.invalidate();
+			response.sendRedirect("Login.jsp");
+		}
+		else
+		{
+			
+			
+			RequestDispatcher rd = request.getRequestDispatcher("ChangePassword.jsp");
+			rd.include(request, response);
+			printer.close();
+			return;
+		}
+	}
+
+}
